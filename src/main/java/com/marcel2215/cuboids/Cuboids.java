1adccaa3ffba9693cc6b4c98f54d6a8d4d3a5470
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-06-07 18:41:10 +0200

    Prevent using buckets in cuboid-protected areas

--- src/main/java/com/marcel2215/cuboids/Cuboids.java
+++ src/main/java/com/marcel2215/cuboids/Cuboids.java
@@ -1,22 +1,33 @@
 package com.marcel2215.cuboids;
 
-import net.fabricmc.api.*;
-import net.fabricmc.fabric.api.event.player.*;
-import net.minecraft.block.*;
-import net.minecraft.block.entity.*;
-import net.minecraft.entity.*;
-import net.minecraft.entity.player.*;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MarkerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.text.Text;
-import net.minecraft.util.*;
-import net.minecraft.util.hit.*;
-import net.minecraft.util.math.*;
-import net.minecraft.world.*;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
 
 public class Cuboids implements ModInitializer {
     @Override
     public void onInitialize() {
         UseBlockCallback.EVENT.register(this::onBlockUse);
         PlayerBlockBreakEvents.BEFORE.register(this::onBlockBreak);
+        UseItemCallback.EVENT.register(this::onItemUse);
     }
 
     private ActionResult onBlockUse(PlayerEntity playerEntity, World world, Hand hand, BlockHitResult blockHitResult) {
@@ -34,7 +45,7 @@ public class Cuboids implements ModInitializer {
             return ActionResult.PASS;
         }
 
-        var markers = world.getEntitiesByType(EntityType.MARKER, new Box(blockHitResult.getBlockPos()).expand(1), e-> true);
+        var markers = world.getEntitiesByType(EntityType.MARKER, new Box(blockHitResult.getBlockPos()).expand(1), e -> true);
         var marker = markers.stream().findFirst().orElse(null);
 
         if (marker == null) {
@@ -93,7 +104,7 @@ public class Cuboids implements ModInitializer {
             return tags.contains("__type__cuboid");
         });
 
-        for (var marker: markers) {
+        for (var marker : markers) {
             var tags = marker.getCommandTags();
             var playerUUID = player.getUuidAsString();
 
@@ -103,5 +114,22 @@ public class Cuboids implements ModInitializer {
         }
 
         return false;
+    }
+
+    private TypedActionResult<ItemStack> onItemUse(PlayerEntity playerEntity, World world, Hand hand) {
+        var itemStack = playerEntity.getStackInHand(hand);
+        var item = itemStack.getItem();
+
+        if (item == Items.BUCKET || item == Items.WATER_BUCKET || item == Items.LAVA_BUCKET) {
+            var blockHitResult = (BlockHitResult) playerEntity.raycast(5.0D, 1.0F, true);
+            if (blockHitResult.getType() == BlockHitResult.Type.BLOCK) {
+                if (isBuildingBlocked(world, blockHitResult.getBlockPos(), playerEntity, 50)) {
+                    playerEntity.sendMessage(Text.of("Protected by Cuboid"), true);
+                    return new TypedActionResult<>(ActionResult.FAIL, itemStack);
+                }
+            }
+        }
+
+        return new TypedActionResult<>(ActionResult.PASS, itemStack);
     }
 }
