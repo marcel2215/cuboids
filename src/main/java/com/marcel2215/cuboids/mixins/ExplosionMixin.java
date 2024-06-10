b4785209c2054a0e7be945f5583ca020c83f0070
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-06-11 01:47:16 +0200

    Fix explosions not removing cuboid markers

--- src/main/java/com/marcel2215/cuboids/mixins/ExplosionMixin.java
+++ src/main/java/com/marcel2215/cuboids/mixins/ExplosionMixin.java
@@ -16,18 +16,24 @@ public class ExplosionMixin {
     @Inject(method = "affectWorld", at = @At("HEAD"))
     private void onAffectWorld(boolean bl, CallbackInfo ci) {
         var explosion = (Explosion) (Object) this;
-        var causingEntity = explosion.getCausingEntity();
+        var entity = explosion.getEntity();
 
-        if (causingEntity == null) {
+        if (entity == null) {
             return;
         }
 
-        var world = causingEntity.getWorld();
+        var world = entity.getWorld();
         for (BlockPos pos : explosion.getAffectedBlocks()) {
             var block = world.getBlockState(pos).getBlock();
             if (block == Blocks.DIAMOND_BLOCK) {
-                var markers = world.getEntitiesByType(EntityType.MARKER, new Box(pos).expand(1), e-> true);
-                markers.stream().findFirst().ifPresent(marker -> marker.remove(Entity.RemovalReason.KILLED));
+                var markers = world.getEntitiesByType(EntityType.MARKER, new Box(pos).expand(2), e -> {
+                    var tags = e.getCommandTags();
+                    return tags.contains("__type__cuboid");
+                });
+
+                for (var marker: markers) {
+                    marker.remove(Entity.RemovalReason.KILLED);
+                }
             }
         }
     }
